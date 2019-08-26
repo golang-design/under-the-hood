695e79fb561d17f4b60295d573ef3e603bb3a8f0
Changkun Ou <hi@changkun.us>
2019-08-26 11:12:07 +0200

    book: errors inspection

--- gosrc/errors/wrap.go
+++ gosrc/errors/wrap.go
@@ -8,9 +8,8 @@ import (
 	"internal/reflectlite"
 )
 
-// Unwrap returns the result of calling the Unwrap method on err, if err's
-// type contains an Unwrap method returning error.
-// Otherwise, Unwrap returns nil.
+// Unwrap 返回在 err 上调用 Unwrap 方法的结果，如果错误的类型包含一个 Unwrap 方法则返回错误。
+// 否则，Unwrap 返回 nil。
 func Unwrap(err error) error {
 	u, ok := err.(interface {
 		Unwrap() error
@@ -21,13 +20,11 @@ func Unwrap(err error) error {
 	return u.Unwrap()
 }
 
-// Is reports whether any error in err's chain matches target.
-//
-// The chain consists of err itself followed by the sequence of errors obtained by
-// repeatedly calling Unwrap.
-//
-// An error is considered to match a target if it is equal to that target or if
-// it implements a method Is(error) bool such that Is(target) returns true.
+// Is 报告错误链中的任何错误是否与目标匹配。
+// 该链由错误本身和随后通过反复调用 Unwrap 的得到的错误序列组成。
+// 如果错误等于该目标或错误，或者当该错误实现了
+// 方法 Is(error)bool，使 Is(target) 返回 true。
+// 则认为该错误与目标匹配
 func Is(err, target error) bool {
 	if target == nil {
 		return err == target
@@ -63,6 +60,17 @@ func Is(err, target error) bool {
 //
 // As will panic if target is not a non-nil pointer to either a type that implements
 // error, or to any interface type. As returns false if err is nil.
+// As 找到 err 链中与 target 匹配的第一个错误，
+// 如果是，则将 target 设置为该错误值并返回 true。
+//
+// 该链由 err 本身组成，后面是通过重复调用 Unwrap 获得的错误序列。
+//
+// 如果错误的具体值可分配给 target 指向的值，
+// 或者错误的方法为 As(interface {}) bool 使得 As(target) 返回 true，则错误匹配目标。
+// 在后一种情况下，As 方法负责设置目标。
+//
+// 如果 target 不是指向实现错误的类型或任何接口类型的非零指针，则会发生混乱。
+// 如果错误为 nil ，则返回 false。
 func As(err error, target interface{}) bool {
 	if target == nil {
 		panic("errors: target cannot be nil")
