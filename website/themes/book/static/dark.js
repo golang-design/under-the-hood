003bcae865542fe6569c4b9b8d8d64eae7f09d40
Changkun Ou <hi@changkun.us>
2020-03-03 19:38:14 +0100

    website: set preference to local storage
    Fixes #34

--- website/themes/book/static/dark.js
+++ website/themes/book/static/dark.js
@@ -1,9 +1,19 @@
-var enabled = false
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  enabled = true; enable(); document.getElementById("dark-mode-checkbox").checked = true;
+var enabled = localStorage.getItem('dark-mode')
+if (enabled === null) {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    enable(); document.getElementById("dark-mode-checkbox").checked = true;
+  }
+} else if (enabled === 'true') {
+  enable(); document.getElementById("dark-mode-checkbox").checked = true;
 }
-function enable()  {DarkReader.enable({brightness: 100, contrast: 85, sepia: 10});}
-function disable() {DarkReader.disable();}
+function enable()  {
+  DarkReader.enable({brightness: 100, contrast: 85, sepia: 10});
+  localStorage.setItem('dark-mode', 'true');
+}
+function disable() {
+  DarkReader.disable();
+  localStorage.setItem('dark-mode', 'false')
+}
 function darkmode() {
-  if (!enabled) { enable(); enabled = true;} else { disable(); enabled = false;}
+  if (localStorage.getItem('dark-mode') === 'false') { enable(); } else { disable();}
 }
